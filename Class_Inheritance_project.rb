ee68f7e092ac897a4ff1474152af68536fa5f8a5
rlachivirus <rlachivirus@gmail.com>
2021-08-31 14:33:17 -0400

    adding employee

--- Class_Inheritance_project.rb
+++ Class_Inheritance_project.rb
@@ -1,16 +1,38 @@
 class Employee
+
     attr_reader :name, :title, :salary, :boss
+
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
     end
+    
+    def bonus(multiplier)
+        bonus = (self.salary) * multiplier
+    end
+
 end
 
 class Manager < Employee
-    def initialize
-        @employee = []
+
+    attr_reader :employees
+
+    def initialize(name, title, salary, boss, *employees)
+        @employee = employees
+
         super(name, title, salary, boss)
     end
+
+    # def add_employee(employee)
+    #     @employees << employee
+    # end
+
+    def bonus(multiplier)
+        sum = 0
+        self.employees.each { |employee| sum += employee.salary }
+        bonus = sum * multiplier
+    end
+
 end
